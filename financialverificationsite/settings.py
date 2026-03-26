0039248177e6fcdadd89c961999ad5823ef5837a
aljoufi0 <algwfy8@gmail.com>
2026-03-26 09:01:27 +0300

    upgrade

--- financialverificationsite/settings.py
+++ financialverificationsite/settings.py
@@ -89,12 +89,8 @@ WSGI_APPLICATION = 'financialverificationsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'financialverificationsitedb',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
